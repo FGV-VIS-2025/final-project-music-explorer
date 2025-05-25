f2dd2d333d284346b71ed42dd3ec459d574384bd
pedrotokar <pedrotokar2004@gmail.com>
2025-05-25 15:55:48 -0300

    Add files and directory to data generation python scripts

---
+++ extras/musicbrainz-scrapper.py
@@ -0,0 +1,58 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+from tqdm import tqdm
+import json
+from pprint import pprint
+
+url = "https://musicbrainz.org/genres"
+url_genre = "https://musicbrainz.org"
+page = urlopen(url)
+
+html_bytes = page.read()
+html = html_bytes.decode("ISO-8859-1")
+
+soup = BeautifulSoup(html, "html.parser")
+
+links = soup.find("div", id="content").find_all("li")
+
+genres_dict = dict()
+
+def extract(link):
+    if link:
+        content = link.find_next("td").find_all("a")
+        content = [link.text for link in content]
+        return content
+    return None
+
+def get_info(link):
+    page_genre = urlopen(link)
+    html_genre = page_genre.read().decode()
+    soup_genre = BeautifulSoup(html_genre, "html.parser")
+
+    info = {"ref": link}
+
+    link_from = soup_genre.find("th", string="from:")
+    link_parent = soup_genre.find("th", string="subgenre of:")
+    link_fusion = soup_genre.find("th", string="fusion of:")
+    link_influeces = soup_genre.find("th", string="influenced by:")
+
+    info["from"] = extract(link_from)
+    info["parent"] = extract(link_parent)
+    info["fused"] = extract(link_fusion)
+    info["influence"] = extract(link_influeces)
+
+    return info
+
+for i in tqdm(range(len(links))):
+    href_genre = links[i].find("a")["href"]
+    genre_name = links[i].get_text()
+    link = url_genre + href_genre
+    genres_dict[genre_name] = get_info(link)
+
+    # if i > 10:
+    #     break
+
+json_genres = json.dumps(genres_dict, indent=4, ensure_ascii=False)
+with open("genres.json", "w", encoding="utf-8") as f:
+    f.write(json_genres)
---
+++ extras/process.py
@@ -0,0 +1,130 @@
+#progressbar
+from tqdm import tqdm
+#File opening
+import tarfile
+import subprocess
+#Data processing
+import json
+from collections import defaultdict
+from pprint import pprint
+
+#Different outputs of the script
+filtered = list()
+filtered_rock = list()
+filtered_only_features = list()
+artist_map = dict()
+
+#Counting hash maps
+counts = defaultdict(lambda: 0)
+counts_rock = defaultdict(lambda: 0)
+
+
+#It isnt possible to safely open the file with native python tools because
+#we get a buffer in which we cant use seek, and so the python standard features
+#to iter in the buffer will error out. So the approach is to use shell commands.
+#they are wrapped here into an iterator to convenience
+def iter_in_file(tar_name, file_name):
+    if not tarfile.is_tarfile(tar_name):
+        print(tar_name, "isn't a tar xz file!")
+        exit()
+    xz_call = subprocess.Popen(
+        ["xz", "-dc", tar_name], #The command here is xz --decompress --stdout "tar file"
+        stdout = subprocess.PIPE
+    )
+
+    tar_call = subprocess.Popen(
+        ["tar", "-xO", "--file=-", file_name], #The command here is tar -x0 --file="the file inside thar"
+        stdin = xz_call.stdout,
+        stdout = subprocess.PIPE
+    )
+
+    xz_call.stdout.close()
+
+    for index, line in enumerate(tar_call.stdout):
+        yield index, line
+
+    print("Closing subprocesses and pipes")
+    tar_call.stdout.close()
+    tar_call.wait()
+    xz_call.wait()
+
+for index, line in tqdm(iter_in_file("tarxz/artist.tar.xz", "mbdump/artist")):
+    line = line.decode("utf-8", errors="replace").rstrip("\n")
+    dictionary = json.loads(line)
+    if(len(dictionary["relations"]) > 0):
+        entry = {
+            "n": dictionary["name"],
+            "ms": set(),
+            "im": set()
+        }
+        for relation in dictionary["relations"]:
+            if relation["target-type"] == "artist" and relation["type"] == "member of band":
+                if relation["direction"] == "foward" and relation["artist"]["id"] not in entry["im"]:
+                    entry["im"].add(relation["artist"]["id"])
+                elif relation["direction"] == "backward" and relation["artist"]["id"] not in entry["ms"]:
+                    entry["ms"].add(relation["artist"]["id"])
+        entry["ms"] = list(entry["ms"])
+        entry["im"] = list(entry["im"])
+        artist_map[dictionary["id"]] = entry
+
+    # generos = dictionary.get("genres", list())
+    # if len(generos) != 0:
+    #     filtered.append(dictionary)
+    #     new = {
+    #         "genres": generos,
+    #         "life-span": dictionary["life-span"],
+    #         "name": dictionary["name"]
+    #     }
+    #     filtered_only_features.append(new)
+    #     generos = [genero["name"] for genero in dictionary["genres"]]
+    #     print(generos)
+    #     tem_rock = 0
+    #     for genero in generos:
+    #         if "rock" in genero:
+    #             counts_rock[genero] += 1
+    #             tem_rock = 1
+    #     if tem_rock:
+    #         filtered_rock.append(dictionary)
+
+print("Writing artists map to json")
+with open("output/artist_map.json", "w") as f:
+    json.dump(artist_map, f, indent = 2)
+
+# pprint(counts)
+# pprint(counts_rock)
+# print(index)
+# print("escrevendo artistas")
+# with open("artists.json", "w") as f:
+#     json.dump(filtered, f)
+# print("escrevendo artistas só três featuress")
+# with open("features_artists.json", "w") as f:
+#     json.dump(filtered_only_features, f, indent = 2)
+# print("escrevendo artistas de rock")
+# with open("rock_artists.json", "w") as f:
+#     json.dump(filtered_rock, f)
+# print("escrevendo counts")
+# with open("counts.json", "w") as f:
+#     json.dump(counts, f, indent = 2)
+# print("escrevendo counts de rock")
+# with open("rock_counts.json", "w") as f:
+#     json.dump(counts_rock, f, indent = 2)
+
+
+#
+# with tarfile.open("artist.tar.xz", "r|xz") as compacted:
+#     artists = compacted.getmember("mbdump/artist")
+#     print("Got member info of artist file")
+#     artists = compacted.extractfile(artists)
+#     print("Got file representation of extractedfile")
+#     read_buffer = b""
+#     while True:
+#         chunk = artists.read1(1024 * 1024 * 100) #100 MiB per read - since I have memory :D
+#         if not chunk:
+#             break
+#         print(chunk)
+#
+#
+# exit()
+#
+# with open("artist", "r") as f:
+#     for index, line in enumerate(f):
