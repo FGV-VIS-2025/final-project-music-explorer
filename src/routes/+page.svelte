07a908acdd450aa6a02443386588036a851f1e08
ddanieldma <danielalmemiranda@gmail.com>
2025-06-15 21:32:27 -0300

    implements sliding sidebar and organizes main page code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,6 @@
     import SearchArtist from "$lib/components/selectors/get-artist.svelte";
     import ExpandedStack from "$lib/components/selectors/expanded-stack.svelte";
     import ArtistInfo from "$lib/components/artist-info.svelte";
-    import { image } from "d3";
 
     //Search variables
     let searchArtist;
@@ -16,154 +15,115 @@
 
     //artist info sidebar related variables
     let nodeMap = new Map();
-    let displayInfo = true;
+    let displayInfo = true; // Sidebar starts as visible
     let infoArtist;
     let forceHighlight;
 
     $: console.log("info:", infoArtist);
-
-    $: infoDisplay = displayInfo ? "block" : "none";
 
     function showInfo() {
         displayInfo = !displayInfo;
     }
 </script>
 
-<!-- <h1>Music explorer</h1> -->
+<div class="app-container">
+    <div class="main-content">
+        <SearchArtist bind:artistId={searchArtist} bind:searching={expanding}/>
+        <ExpandedStack bind:expandedNodes
+                       bind:removeNode
+                       bind:expanding
+                       bind:selectedNode={infoArtist}/>
+        <ArtistGraph importArtist={searchArtist}
+                     removeArtist={removeNode}
+                     bind:expandedNodes
+                     bind:expanding
+                     bind:nodeMap
+                     bind:selectedNode={infoArtist}
+                     bind:activeLegendItems={activeLegendItems}
+                     forceHighlight={forceHighlight}
+                     selectedNodeId={infoArtist ? infoArtist.id : null}/>
+    </div>
 
-<SearchArtist bind:artistId={searchArtist} bind:searching={expanding}/>
-<ExpandedStack bind:expandedNodes
-               bind:removeNode
-               bind:expanding
-			   bind:selectedNode={infoArtist}/>
-<ArtistGraph importArtist={searchArtist}
-             removeArtist={removeNode}
-             bind:expandedNodes
-             bind:expanding
-             bind:nodeMap
-             bind:selectedNode={infoArtist}
-             bind:activeLegendItems={activeLegendItems}
-             forceHighlight={forceHighlight}
-			 selectedNodeId={infoArtist ? infoArtist.id : null}/>
+    <div id="artist-info-container" class:closed={!displayInfo}>
+        <button on:click={showInfo} id="display-button">
+            {#if displayInfo}
+                <img src={`icons/close.svg`} height="20rem" alt="close">
+            {:else}
+                <img src={`icons/chevron_left.svg`} height="20rem" alt="show">
+            {/if}
+        </button>
 
-<button on:click={showInfo} id="display-button" style="border: none;">
-    {#if displayInfo}
-        <img src={`icons/close.svg`} height="20rem" alt="close">
-    {:else}
-        <img src={`icons/chevron_left.svg`} height="20rem" alt="show">
-    {/if}
-</button>
-             
-<div id="artist-info" style="display: {infoDisplay};">
-    <ArtistInfo artist={infoArtist}
-                activeLegend={activeLegendItems}
-                nodeMap={nodeMap}
-                expanding={expanding}
-                bind:barHighlight={forceHighlight}
-    />
+        <div class="artist-info-content">
+            <ArtistInfo artist={infoArtist}
+                        activeLegend={activeLegendItems}
+                        nodeMap={nodeMap}
+                        expanding={expanding}
+                        bind:barHighlight={forceHighlight}
+            />
+        </div>
+    </div>
 </div>
-<!---->
-<!-- <div id="chart-selector"> -->
-<!-- 	<ul> -->
-<!-- 		<li> -->
-<!-- 			<input type="radio" name="chart" id="chart1"> -->
-<!-- 			<label for="chart1">chart1</label> -->
-<!-- 		</li> -->
-<!-- 		<li> -->
-<!-- 			<input type="radio" name="chart" id="chart2"> -->
-<!-- 			<label for="chart2">chart2</label> -->
-<!-- 		</li> -->
-<!-- 		<li> -->
-<!-- 			<input type="radio" name="chart" id="chart3"> -->
-<!-- 			<label for="chart3">chart3</label> -->
-<!-- 		</li> -->
-<!-- 		<li> -->
-<!-- 			<input type="radio" name="chart" id="chart4"> -->
-<!-- 			<label for="chart4">chart4</label> -->
-<!-- 		</li> -->
-<!---->
-<!-- 	</ul> -->
-<!-- </div> -->
-<!---->
+
+
 <style>
-	#chart-selector {
-		position: fixed;
-		top : 120px;
-		right: 40px;
-		display: flex;
-		align-items: center;
-		border: 1px solid antiquewhite;
-		border-radius: 4px;
-		width: 500px;
-		height: 40px;
-	}
+    :global(body) {
+        margin: 0;
+        /* We no longer need overflow-x here, it's better on the container */
+    }
 
-	#chart-selector ul {
-		list-style-type: none;
-		padding: 0;
-		margin: 0;
-		display: flex;
-		width: 90%;
-		height: 100%;
-		justify-content: space-around;
-		align-items: center;
-	}
+    .app-container {
+        /* This is now the positioning context for the sidebar */
+        position: relative;
+        width: 100vw;
+        height: 100vh;
+        /* This prevents a scrollbar if any weirdness happens */
+        overflow: hidden;
+    }
 
-	#chart-selector li {
-		height: 100%;
-		float: left;
-		flex-grow: 1;
-		text-align: center;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-
-		background-color: blue;
-	}
-
-	#chart-selector label {
-		cursor: pointer;
-		height: 100%;
-		display: flex;
-		text-align: center;
-		justify-content: center;
-	}
-
-	#chart-selector input[type="radio"] {
-		appearance: none;
-	}
-
-	#chart-selector input[type="radio"]:checked+label {
-		background-color: yellow;
-	}
-	
-    #display-button {
-        position: fixed;
-        top: 130px;
-        right: 45px;
-        z-index: 1000;
-		cursor: pointer;
-		display: flex;
+    .main-content {
+        /* This now takes up the full space, all the time */
+        width: 100%;
+        height: 100%;
+        position: relative;
     }
     
-    #artist-info {
-        position: fixed;
-        top: 120px;
-        bottom: 40px;
-        right: 40px;
-        z-index: 999;
+    #artist-info-container {
+        /* === This is the core of the new logic === */
+        position: absolute;
+        top: 0;
+        right: 0;
+        height: 100%;
+        z-index: 10; /* Make sure it renders ON TOP of the main content */
 
-        background-color: var(--accent-black);
-        border-radius: 8px;
-        border: 2px solid rgba(100, 100, 100, 0.6);
+        width: 510px;
+        margin-top: 100px;
+        background-color: #2a2a2a;
+        
+        /* We go back to animating transform, which is perfect for overlays */
+        transform: translateX(0);
+        transition: transform 0.35s ease-in-out;
+    }
 
-        padding: 10px;
-        box-shadow: 0 0px 5px rgba(256, 256, 256, 0.25);
-        /* height: 90%; */
-        width: 500px;
-        box-sizing: border-box;
+    /* When closed, we move it 100% of its own width to the right */
+    #artist-info-container.closed {
+        transform: translateX(100%);
+    }
 
-        overflow-y: scroll;
-        overflow-x: hidden;
+    .artist-info-content {
+        padding: 1rem;
+        height: 100%;
+        overflow-y: auto;
+    }
+
+    #display-button {
+        position: absolute;
+        top: 10px;
+        left: -45px;
+        z-index: 10;
+        border: none;
+        background-color: #333;
+        padding: 8px 5px;
+        border-radius: 5px 0 0 5px;
+        cursor: pointer;
     }
 </style>
